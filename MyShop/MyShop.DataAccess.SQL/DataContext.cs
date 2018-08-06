3336861660ee25af491a2cf5f8354d0c8f2cf4fd
Richard Kasavuli <kasavuli.rm@hotmail.com>
2018-08-06 21:46:59 +0200

    Added Entity Framework

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext: DbContext
+    {
+        public DataContext()
+            :base("DefaultConnection") {//read from webconfig or appconfig 
+        }
+
+        //create database sets
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> Productcategories { get; set; }
+    }
+}
