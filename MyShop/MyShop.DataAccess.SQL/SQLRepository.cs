84f1b97e1b06bd30de9dbabbbcaef46a4f5d1afc
Richard Kasavuli <kasavuli.rm@hotmail.com>
2018-08-06 22:10:01 +0200

    8.2.1.Started SQLRepo

---
+++ MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,47 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+
+        public IQueryable<T> Collection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
