8238990adb47a5e6c94cec839427c5aee668533a
Richard K <37063383+RMKasavuli@users.noreply.github.com>
2018-08-07 08:20:00 +0200

    Ignore these changes

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext: DbContext
+    {
+        public DataContext ()
+            : base("DefaultConnection")//to look in webconfig for the DefaultConnection string 
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategoriess { get; set; }
+    }
+}
