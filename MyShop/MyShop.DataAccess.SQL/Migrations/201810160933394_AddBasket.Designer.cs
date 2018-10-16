12c5e3ae39828e4d5f3f6154f618414d83168124
Richard K <37063383+RMKasavuli@users.noreply.github.com>
2018-10-16 16:33:45 +0200

    Started working on the customer credentials

--- MyShop/MyShop.DataAccess.SQL/Migrations/201810160933394_AddBasket.Designer.cs
+++ MyShop/MyShop.DataAccess.SQL/Migrations/201810160933394_AddBasket.Designer.cs
@@ -7,13 +7,13 @@ namespace MyShop.DataAccess.SQL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class Initial : IMigrationMetadata
+    public sealed partial class AddBasket : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(AddBasket));
         
         string IMigrationMetadata.Id
         {
-            get { return "201810131343132_Initial"; }
+            get { return "201810160933394_AddBasket"; }
         }
         
         string IMigrationMetadata.Source
