6824dbd80011e8a8c7dda267a49340d2f85f798e
Richard K <37063383+RMKasavuli@users.noreply.github.com>
2018-12-05 09:36:06 +0200

    Quick fix in product datacontext class

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -11,14 +11,14 @@ namespace MyShop.DataAccess.SQL
     public class DataContext: DbContext
     {
         public DataContext ()
-            : base("DefaultConnection")//to look in webconfig for the DefaultConnection string 
+            : base("DefaultConnection")//to look in webconfig for the DefaultConnection string //
         {
             
         }
 
         //Add models to the database
         public DbSet<Product> Products { get; set; }
-        public DbSet<ProductCategory> ProductCategoriess { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<BasketItem> BasketItems { get; set; }
         public DbSet<Customer> Customers { get; set; }
